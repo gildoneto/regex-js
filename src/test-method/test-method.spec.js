5beb0ad235b0f3368cdbdabae7d9545b56bf7e80
Gildo Neto <gildo.neto@telus.com>
2022-10-06 09:50:41 -0300

    ignore case tests

--- src/test-method/test-method.spec.js
+++ src/test-method/test-method.spec.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-undef */
 
-describe('Test method', () => {
+describe('test() method', () => {
   describe('match literal', () => {
     it('/Hello/ should should match the string "Hello, World!"', () => {
       const someString = 'Hello, World!';
@@ -15,36 +15,45 @@ describe('Test method', () => {
       expect(result).toBe(false);
     });
   });
-  describe('OR operator => |', () => {
+  describe('OR operator => /dog|cat|bird|fish/', () => {
     const petRegex = /dog|cat|bird|fish/;
-    it('/dog|cat|bird|fish/ should match the string "John has a pet dog."', () => {
+    it('should match the string "John has a pet dog."', () => {
       expect(petRegex.test('John has a pet dog.')).toBe(true);
     });
-    it('/dog|cat|bird|fish/ should NOT match the string "Emma has a pet rock."', () => {
+    it('should NOT match the string "Emma has a pet rock."', () => {
       expect(petRegex.test('Emma has a pet rock.')).toBe(false);
     });
-    it('/dog|cat|bird|fish/ should match the string "Emma has a pet bird."', () => {
+    it('should match the string "Emma has a pet bird."', () => {
       expect(petRegex.test('Emma has a pet bird.')).toBe(true);
     });
-    it('/dog|cat|bird|fish/ should match the string "Liz has a pet cat."', () => {
+    it('should match the string "Liz has a pet cat."', () => {
       expect(petRegex.test('Liz has a pet cat.')).toBe(true);
     });
-    it('/dog|cat|bird|fish/ should NOT match  the string "Kara has a pet dolphin."', () => {
+    it('should NOT match  the string "Kara has a pet dolphin."', () => {
       expect(petRegex.test('Kara has a pet dolphin.')).toBe(false);
     });
-    it('/dog|cat|bird|fish/ should match the string "Alice has a pet fish."', () => {
+    it('should match the string "Alice has a pet fish."', () => {
       expect(petRegex.test('Alice has a pet fish.')).toBe(true);
     });
-    it('/dog|cat|bird|fish/ should NOT match the string "Jimmy has a pet"', () => {
+    it('should NOT match the string "Jimmy has a pet"', () => {
       expect(petRegex.test('Jimmy has a pet')).toBe(false);
     });
   });
 
-  describe('ignore case', () => {
+  describe('ignore case => /javascript/i', () => {
     const ignoreCaseRegex = /javascript/i;
 
-    it('"/javascript/i" should match the string "RegexJavaScriptTips"', () => {
+    it('should match the string "RegexJavaScriptTips"', () => {
       expect(ignoreCaseRegex.test('RegexJavaScriptTips')).toBe(true);
+    });
+    it('should match the string "rEgeXjaVascriPttips"', () => {
+      expect(ignoreCaseRegex.test('rEgeXjaVascriPttips')).toBe(true);
+    });
+    it('should match the string "ReGeXjaVaScRipTTips"', () => {
+      expect(ignoreCaseRegex.test('ReGeXjaVaScRipTTips')).toBe(true);
+    });
+    it('should NOT match the string "Regex Java Script Tips"', () => {
+      expect(ignoreCaseRegex.test('Regex Java Script Tips')).toBe(false);
     });
   });
 });
